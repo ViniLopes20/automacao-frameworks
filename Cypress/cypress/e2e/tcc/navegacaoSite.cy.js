a07d0bc2ad65cc8f455af06337267b49ea38ac66
Vinicius Lopes <vinicius.lopes@liferay.com>
2023-04-24 19:52:44 -0300

    Adicionar o test para Interagir com o modal de endereços salvos com o Cypress

--- Cypress/cypress/e2e/tcc/navegacaoSite.cy.js
+++ Cypress/cypress/e2e/tcc/navegacaoSite.cy.js
@@ -38,4 +38,52 @@ describe('Testes relacionados com navegação pelo site', () => {
 
       cy.url().should('include', 'AccountFlyout_orders')
     });
+
+    it('Interagir com o modal de endereços salvos', () => {
+      cy.get('#nav-link-accountList').click();
+
+      cy.get('#ap_email').type('test@hotmail.com');
+
+      cy.get('.a-button-inner > #continue').click();
+
+      cy.get('#ap_password').type('test');
+
+      cy.get('#signInSubmit').click();
+
+      const endereco1 = cy.get('#glow-ingress-line2').invoke('text');
+
+      cy.get('#nav-global-location-popover-link').click();
+
+      cy.get(':nth-child(1) > .a-list-item > .a-button').should("exist");
+
+      cy.get(':nth-child(2) > .a-list-item > .a-button').should("exist");
+
+      cy.get(':nth-child(2) > .a-list-item > .a-button').click();
+
+      const endereco2 = cy.get('#glow-ingress-line2').invoke('text');
+      
+      expect(endereco1).not.equal(endereco2);
+
+      cy.get('#nav-global-location-popover-link').click();
+
+      cy.get('#GLUXZipUpdateInput_0').type('50050');
+
+      cy.get('#GLUXZipUpdateInput_1').type('900');
+
+      cy.get('#GLUXZipUpdate').click();
+
+      const endereco3 = cy.get('#glow-ingress-line2').invoke('text');
+
+      expect(endereco2).not.equal(endereco3);
+
+      cy.get('#nav-global-location-popover-link').click();
+
+      cy.get('#GLUXZipUpdateInput_0').clear();
+
+      cy.get('#GLUXZipUpdateInput_0').type('00000');
+
+      cy.get('#GLUXZipUpdate').click();
+
+      cy.get('#GLUXZipError').should('be.visible');
+    });
   });
