d73b2ebaff8e784775b6b78eb24bcac048fe78e7
Vinicius Lopes <vinicius.lopes@liferay.com>
2023-04-24 01:45:06 -0300

    Adicionando o teste para Navegar pelo menu suspenso das opções do usuário no Cypress

--- Cypress/cypress/e2e/tcc/navegacaoSite.cy.js
+++ Cypress/cypress/e2e/tcc/navegacaoSite.cy.js
@@ -4,19 +4,38 @@ describe('Testes relacionados com navegação pelo site', () => {
     });
   
     it('Navegar pelo pelo menu de navegação lateral', () => { 
-        cy.get('#nav-hamburger-menu').click();
+      cy.get('#nav-hamburger-menu').click();
 
-        cy.get('.hmenu-compressed-btn').first().click();
+      cy.get('.hmenu-compressed-btn').first().click();
 
-        cy.contains('Games e Consoles').click();
+      cy.contains('Games e Consoles').click();
 
-        cy.contains('Tudo em Games e Consoles').click();
+      cy.contains('Tudo em Games e Consoles').click();
 
-        cy.title().should('eq', 'Games e Consoles | Amazon.com.br');
+      cy.title().should('eq', 'Games e Consoles | Amazon.com.br');
 
-        cy.get('[selected][value="search-alias=videogames"]').should("exist");
+      cy.get('[selected][value="search-alias=videogames"]').should("exist");
         
-        cy.get('.a-list-item > .a-size-base').should("contain.text", "Games e Consoles");
+      cy.get('.a-list-item > .a-size-base').should("contain.text", "Games e Consoles");
+    });
 
-      });
+    it('Navegar pelo menu suspenso das opções do usuário', () => {
+      cy.get('#nav-link-accountList').click();
+
+      cy.get('#ap_email').type('test@hotmail.com');
+
+      cy.get('.a-button-inner > #continue').click();
+
+      cy.get('#ap_password').type('test');
+
+      cy.get('#signInSubmit').click();
+
+      cy.get('#nav-link-accountList').trigger('mouseover');
+
+      cy.get('[id="nav_prefetch_yourorders"]').click();
+
+      cy.get('h1').should('contain', 'Seus pedidos');
+
+      cy.url().should('include', 'AccountFlyout_orders')
+    });
   });
